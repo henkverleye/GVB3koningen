3e932c6db0d56d5c5310c79d6841cae7e71bbfb8
Henk Verleye <henk@kaa.MSHOME>
2013-01-13 13:27:29 +0100

    maandmenu: toont maar 2 weken, waardoor er meer plaats is voor een groter font

--- menu.php
+++ menu.php
@@ -5,8 +5,12 @@ try
 
 	require_once("BaseDocument.php");
 
+	if (function_exists("date_default_timezone_set"))
+		date_default_timezone_set('Europe/Brussels');
+
 	$maanden = array("", "januari", "februari", "maart", "april", "mei", "juni",
 		"juli", "augustus", "september", "oktober", "november", "december");
+	$numWeken = 2;
 
 	$db_conn = mysql_connect("localhost", "3koningen_be", "wbEwbfgb") or die("could not connect to database server");
 	mysql_select_db("3koningen_be") or die("could not find database");
@@ -14,78 +18,59 @@ try
 	$vandaag = getdate();
 	$thisjaar = $vandaag["year"];
 	$thismaand = $vandaag["mon"];
+	$thisdag = $vandaag["mday"];
+	$thiswdag = $vandaag["wday"];
 
-	$prevjaar = $thisjaar;
-	$prevmaand = $thismaand - 1;
-	if ($prevmaand < 1)
-	{
-		--$prevjaar;
-		$prevmaand = 12;
-	}
+	$eersteMaandagD = mktime() - ($thiswdag % 7 - 1) * (24*60*60);
+	$eersteMaandagDatum = strftime("%Y-%m-%d", $eersteMaandagD);
+	$eersteMaandag = getdate($eersteMaandagD);
+	$laatsteVrijdagD = $eersteMaandagD + ($numWeken * 7 - 3) * (24*60*60);
+	$laatsteVrijdagDatum = strftime("%Y-%m-%d", $laatsteVrijdagD);
+	$laatsteVrijdag = getdate($laatsteVrijdagD);
 
-	$nextjaar = $thisjaar;
-	$nextmaand = $thismaand - 1;
-	if ($nextmaand > 12)
-	{
-		++$nextjaar;
-		$nextmaand = 1;
-	}
-	$eerstemaanddag = mktime(12,0,0,$thismaand,1,$thisjaar);
-	$prevmaanddagen = array();
-	while (true)
-	{
-		$eerstemaanddag -= 24 * 60 * 60;
-		$diedag = getdate($eerstemaanddag);
-		$wday = $diedag["wday"];
-		if (($wday == 0) || ($wday == 6) || ($wday == 7)) // zondag
-			break;
-		if (($wday == 1) || ($wday == 2) || ($wday == 4) || ($wday == 5))
-			$prevmaanddagen[] = $diedag["mday"];
-	}
-
-	$laatstemaanddag = mktime(12,0,0,$nextmaand,1,$nextjaar) - 24 * 60 * 60;
-	$nextmaanddagen = array();
-	while (true)
-	{
-		$laatstemaanddag += 24 * 60 * 60;
-		$diedag = getdate($laatstemaanddag);
-		$wday = $diedag["wday"];
-		if (($wday == 0) || ($wday == 6) || ($wday == 7)) // zondag
-			break;
-		if (($wday == 1) || ($wday == 2) || ($wday == 4) || ($wday == 5))
-			$nextmaanddagen[] = $diedag["mday"];
-	}
-
-	$query = "SELECT year(datum), month(datum), dayofmonth(datum), menu FROM maandmenu WHERE (((year(datum) = $thisjaar) AND (month(datum) = $thismaand))";
-	if (count($prevmaanddagen) > 0)
-	{
-		sort($prevmaanddagen);
-		$first = $prevmaanddagen[0];
-		$last = end($prevmaanddagen);
-		$query .= " || ((year(datum) = $prevjaar) AND (month(datum) = $prevmaand) AND (dayofmonth(datum) >= $first) AND (dayofmonth(datum) <= $last))";
-	}
-	if (count($nextmaanddagen) > 0)
-	{
-		sort($nextmaanddagen);
-		$first = $nextmaanddagen[0];
-		$last = end($nextmaanddagen);
-		$query .= " || ((year(datum) = $nextjaar) AND (month(datum) = $nextmaand) AND (dayofmonth(datum) >= $first) AND (dayofmonth(datum) <= $last))";
-	}
-	$query .= ") ORDER BY datum";
+	$menus = array();
+	$query = "SELECT year(datum), month(datum), dayofmonth(datum), menu FROM maandmenu WHERE ((datum >= '$eersteMaandagDatum') AND (datum <= '$laatsteVrijdagDatum')) ORDER by datum";
 	$result = mysql_query($query);
 	if (!$result)
 		echo "query failed";
-	$menus = array();
 	while (list($jaar, $maand, $maanddag, $menu) = mysql_fetch_array($result))
 	{
 		$datum = sprintf("%04d-%02d-%02d", $jaar, $maand, $maanddag);
 		$menus[$datum] = $menu;
 	}
 
-	$doc = CreateBaseDocument("Maandmenu: {$maanden[$thismaand]} $thisjaar", "maandmenu", "schoolmenu");
+	$titel = "Maandmenu: ";
+	if ($eersteMaandag["mon"] == $laatsteVrijdag["mon"])
+	{
+		$dag1 = $eersteMaandag["mday"];
+		$dag2 = $laatsteVrijdag["mday"];
+		$maand = $maanden[$eersteMaandag["mon"]];
+		$jaar = $eersteMaandag["year"];
+		$titel .= "$dag1 - $dag2 $maand $jaar";
+	}
+	else if ($eersteMaandag["year"] == $laatsteVrijdag["year"])
+	{
+		$dag1 = $eersteMaandag["mday"];
+		$dag2 = $laatsteVrijdag["mday"];
+		$maand1 = $maanden[$eersteMaandag["mon"]];
+		$maand2 = $maanden[$laatsteVrijdag["mon"]];
+		$jaar = $eersteMaandag["year"];
+		$titel .= "$dag1 $maand1 - $dag2 $maand2 $jaar";
+	}
+	else
+	{
+		$dag1 = $eersteMaandag["mday"];
+		$dag2 = $laatsteVrijdag["mday"];
+		$maand1 = $maanden[$eersteMaandag["mon"]];
+		$maand2 = $maanden[$laatsteVrijdag["mon"]];
+		$jaar1 = $eersteMaandag["year"];
+		$jaar2 = $laatsteVrijdag["year"];
+		$titel .= "$dag1 $maand1 $jaar1- $dag2 $maand2 $jaar2";
+	}
+	$doc = CreateBaseDocument($titel, "maandmenu", "schoolmenu");
 	$content = $doc->FindElementById("content");
 	{
-		$content->AddTag("h1", array(), "{$maanden[$thismaand]} $thisjaar");
+		$content->AddTag("h1", array(), $titel);
 
 		$table = $content->AddTag("table", array("id" => "maandmenu", "summary" => "maandmenu"));
 		{
@@ -97,43 +82,23 @@ try
 
 			$tbody = $table->AddTag("tbody");
 			{
-				$weektimestamp = mktime(12,0,0,$thismaand,1,$thisjaar);
-				$weekdate = getdate($weektimestamp);
-				while (($weekdate["wday"] != 0) && ($weekdate["wday"] != 6) && ($weekdate["wday"] != 7))
-				{
-					$weektimestamp -= 24 * 60 * 60;
-					$weekdate = getdate($weektimestamp);
-				}
-				while ($weekdate["wday"] != 1)
-				{
-					$weektimestamp += 24 * 60 * 60;
-					$weekdate = getdate($weektimestamp);
-				}
-
-				while (($weekdate["mon"] == $thismaand) || ($weekdate["mon"] == $prevmaand))
+				for ($idx = 0; $idx < $numWeken; ++$idx)
 				{
 					$tr = $tbody->AddTag("tr");
-
-					$dagtimestamp = $weektimestamp;
-					$dagdate = $weekdate;
 					foreach (array(1, 2, 4, 5) as $wday)
 					{
-						while ($dagdate["wday"] != $wday)
-						{
-							$dagtimestamp += 24 * 60 * 60;
-							$dagdate = getdate($dagtimestamp);
-						}
-						$datum = sprintf("%04d-%02d-%02d", $dagdate["year"], $dagdate["mon"], $dagdate["mday"]);
+						$dagtimestamp = $eersteMaandagD + ($idx * 7 + $wday - 1) * 24*60*60;
+						$dagdate = getdate($dagtimestamp);
+						$datum = strftime("%Y-%m-%d", $dagtimestamp);
+						$dagtitel = $dagdate["mday"] . " " . $maanden[$dagdate["mon"]];
 						$menu = isset($menus[$datum]) ? $menus[$datum] : "";
 
 						$td = $tr->AddTag("td");
 						{
-							$td->AddTag("h3", array(), $dagdate["mday"]);
+							$td->AddTag("h3", array(), $dagtitel);
 							$td->AddHTMLFragment($menu);
 						}
 					}
-					$weektimestamp += 7 * 24 * 60 * 60;
-					$weekdate = getdate($weektimestamp);
 				}
 			}
 		}
