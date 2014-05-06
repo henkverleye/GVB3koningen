b60d752d363d54ff5ff2dc2a8a6a86b9b9f2620b
Henk Verleye <henk@kaa.MSHOME>
2014-05-06 19:34:18 +0200

    recentste versies

--- fotogalerij.php
+++ fotogalerij.php
@@ -3,23 +3,42 @@ try
 {
 	require_once("BaseDocument.php");
 
-    $doc = CreateBaseDocument("Fotogalerij", "fotogalerij", "fotogalerij");
-    $head = $doc->GetHead();
-    {
-        $head->AddTag("script", array("type" => "text/javascript", "src" => "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"));
-        $head->AddTag("link", array("href" => "pwi/js/jquery.fancybox/jquery.fancybox.css", "rel" => "stylesheet", "type" => "text/css"));
-        $head->AddTag("script", array("src" => "pwi/js/jquery.fancybox/jquery.fancybox.pack.js", "type" => "text/javascript"));
-        $head->AddTag("link", array("href" => "pwi/js/jquery.fancybox/helpers/jquery.fancybox-buttons.css?v=2.0.5", "rel" => "stylesheet", "type" => "text/css"));
-        $head->AddTag("script", array("type" => "text/javascript", "src" => "pwi/js/jquery.fancybox/helpers/jquery.fancybox-buttons.js?v=2.0.5"));
-        $head->AddTag("script", array("src" => "pwi/js/jquery.blockUI.js", "type" => "text/javascript"));
-        $head->AddTag("link", array("href" => "pwi/css/pwi.css", "rel" => "stylesheet", "type" => "text/css"));
-        $head->AddTag("script", array("src" => "pwi/js/jquery.pwi-min.js", "type" => "text/javascript"));
-        $head->AddTag("script", array("type" => "text/javascript"), '$(document).ready(function(){$("#picasacontainer").pwi({username: "gvb3koningen", thumbCrop: 1, thumbSize: 72});});');
-    }
-    $content = $doc->FindElementById("content");
-    {
-        $content->AddTag("div", array("id" => "picasacontainer"));
-    }
+	$doc = CreateBaseDocument("Fotogalerij", "fotogalerij", "fotogalerij");
+	$head = $doc->GetHead();
+	{
+		$head->AddTag("script", array("type" => "text/javascript", "src" => "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"));
+		$head->AddTag("link", array("href" => "pwi/js/jquery.fancybox/jquery.fancybox.css", "rel" => "stylesheet", "type" => "text/css"));
+		$head->AddTag("script", array("src" => "pwi/js/jquery.fancybox/jquery.fancybox.pack.js", "type" => "text/javascript"));
+		$head->AddTag("link", array("href" => "pwi/js/jquery.fancybox/helpers/jquery.fancybox-buttons.css?v=2.0.5", "rel" => "stylesheet", "type" => "text/css"));
+		$head->AddTag("script", array("type" => "text/javascript", "src" => "pwi/js/jquery.fancybox/helpers/jquery.fancybox-buttons.js?v=2.0.5"));
+		$head->AddTag("script", array("src" => "pwi/js/jquery.blockUI.js", "type" => "text/javascript"));
+		$head->AddTag("link", array("href" => "pwi/css/pwi.css", "rel" => "stylesheet", "type" => "text/css"));
+		$head->AddTag("script", array("src" => "pwi/js/jquery.pwi-min.js", "type" => "text/javascript"));
+		$settings = '{
+			username: "gvb3koningen",
+			thumbCrop: 1,
+			albumThumbSize: 72,
+			showAlbumPhotoCount: false,
+			showAlbumDescription: false,
+			labels: {
+				photo: "foto",
+				photos: "fotos",
+				downloadphotos: "Download fotos",
+				albums: "Terug naar albums",
+				page: "Pagina",
+				prev: "Vorige",
+				next: "Volgende",
+				showPermalink: "Show PermaLink",
+				showMap: "Toon kaart",
+				videoNotSupported: "Video niet ondersteund"
+			}
+		}';
+        $head->AddTag("script", array("type" => "text/javascript"), '$(document).ready(function(){$("#picasacontainer").pwi('. $settings . ');});');
+	}
+	$content = $doc->FindElementById("content");
+	{
+		$content->AddTag("div", array("id" => "picasacontainer"));
+	}
 
 	echo $doc->Serialize(true);
 }
--- upload.php
+++ upload.php
@@ -26,16 +26,29 @@ try
 		$head->AddTag("link", array("rel" => "stylesheet", "href" => "http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css"));
 		$head->AddTag("script", array("type" => "text/javascript", "src" => "http://code.jquery.com/jquery-1.8.2.js"));
 		$head->AddTag("script", array("type" => "text/javascript", "src" => "http://code.jquery.com/ui/1.9.1/jquery-ui.js"));
-		$head->AddTag("script", array(), '$(function() { $("#begindatum").datepicker({dateFormat:"yy-mm-dd"}); $("#einddatum").datepicker({dateFormat:"yy-mm-dd"}); });');
+        $head->AddTag("script", array(), 'function ThumbsVisible(){$("#thumbfieldset").css("display", ($("#context:checked").val() == "schat") ? "block" : "none"); return false; } $(function() { $("#begindatum").datepicker({dateFormat:"yy-mm-dd"}); $("#einddatum").datepicker({dateFormat:"yy-mm-dd"}); });');
 	}
 	$content = $doc->FindElementById("content");
-	{
+    {
+        $filefieldTemp = Null;
 		if (isset($_FILES["filefield"]))
-		{
-			$allowedExts = array("PDF");
-			$allowedMime = array("application/pdf");
+        {
+            $filefieldTemp = "uploads/temp/filefield";
+            move_uploaded_file($_FILES['filefield']['tmp_name'], $filefieldTemp));
+        }
+        $thumbfieldTemp = Null;
+		if (isset($_FILES["thumbfield"]))
+        {
+            $thumbfieldTemp = "uploads/temp/thumbfield";
+            move_uploaded_file($_FILES['thumbfield']['tmp_name'], $thumbfieldTemp));
+        }
+        if ($filefieldTemp != Null)
+        {
+			$allowedExts = array("PDF", "JPG");
+			$allowedMime = array("application/pdf", "image/jpeg");
 			$extension = strtoupper(end( explode( ".", $_FILES["filefield"]["name"])));
-			if (in_array($_FILES["filefield"]["type"], $allowedMime) &&
+			$mime = $_FILES["filefield"]["type"];
+			if (in_array($mime, $allowedMime) &&
 				in_array($extension, $allowedExts))
 			{
 				if ( $_FILES['filefield']['error'] > 0)
@@ -50,15 +63,11 @@ try
 					$content->AddTag("p", array(), "Type: {$_FILES['filefield']['type']}");
 					$content->AddTag("p", array(), "Grootte: {$_FILES['filefield']['size']}");
 					$destination = "uploads/$context/{$_FILES['filefield']['name']}";
-					if (move_uploaded_file($_FILES['filefield']['tmp_name'], $destination))
-					{
-						mysql_query("INSERT INTO sp_timestamp (timestamp) VALUES (now())");
-						mysql_query("INSERT INTO sp_file_uploads (timestamp_key, url) VALUES (LAST_INSERT_ID(), '$destination')");
-						mysql_query("INSERT INTO sp_file_context (upload_key, titel, schooljaar, context, begindatum, einddatum) VALUES (LAST_INSERT_ID(), '$titel', $schooljaar, '$context', '$begindatum', '$einddatum')");
-						$content->AddTag("p", array(), "Bewaard als: $destination");
-					}
-					else
-						$content->AddTag("p", array(), "Error: failed to move to $destination");
+					rename($filefieldTemp, $destination);
+                    mysql_query("INSERT INTO sp_timestamp (timestamp) VALUES (now())");
+                    mysql_query("INSERT INTO sp_file_uploads (timestamp_key, url) VALUES (LAST_INSERT_ID(), '$destination')");
+                    mysql_query("INSERT INTO sp_file_context (upload_key, titel, schooljaar, context, begindatum, einddatum) VALUES (LAST_INSERT_ID(), '$titel', $schooljaar, '$context', '$begindatum', '$einddatum')");
+                    $content->AddTag("p", array(), "Bewaard als: $destination");
 				}
 			}
 			else
@@ -69,14 +78,17 @@ try
 		$field = $form->AddTag("fieldset");
 		$field->AddTag("label", array("for" => "filefield"), "File name:");
 		$field->AddTag("input", array("type" => "file", "name" => "filefield", "id" => "filefield"));
+		$field = $form->AddTag("fieldset", array("style" => "display: none;", "id" => "thumbfieldset"));
+		$field->AddTag("label", array("for" => "thumbfield"), "Thumbnail:");
+		$field->AddTag("input", array("type" => "file", "name" => "thumbfield", "id" => "thumbfield"));
 		$field = $form->AddTag("fieldset");
 		$field->AddTag("label", array("for" => "titel"), "Titel:");
 		$field->AddTag("input", array("type" => "text", "name" => "titel", "id" => "titel", "size" => "35", "value" => ""));
 		$field = $form->AddTag("fieldset");
 		$field->AddTag("label", array("for" => "context"), "Context:");
-		$field->AddTag("input", array("type" => "radio", "name" => "context", "id" => "context", "value" => "brief", "checked" => "checked"), "brief");
-		$field->AddTag("input", array("type" => "radio", "name" => "context", "id" => "context", "value" => "schat"), "De Schat");
-		$field->AddTag("input", array("type" => "radio", "name" => "context", "id" => "context", "value" => "weekagenda"), "Weekagenda");
+		$field->AddTag("input", array("type" => "radio", "name" => "context", "id" => "context", "value" => "brief", "checked" => "checked", "onchange" => "return ThumbsVisible();"), "brief");
+		$field->AddTag("input", array("type" => "radio", "name" => "context", "id" => "context", "value" => "schat", "onchange" => "return ThumbsVisible();"), "De Schat");
+		$field->AddTag("input", array("type" => "radio", "name" => "context", "id" => "context", "value" => "weekagenda", "onchange" => "return ThumbsVisible();"), "Weekagenda");
 		$field = $form->AddTag("fieldset");
 		$field->AddTag("label", array("for" => "begindatum"), "Verschijningsdatum:");
 		$field->AddTag("input", array("type" => "text", "name" => "begindatum", "id" => "begindatum", "size" => "12", "value" => $begindatum));
