3afc7ab3a3f905743917f054db25fa8d34f4782f
Henk Verleye <henk@kaa.MSHOME>
2014-08-26 21:29:46 +0200

    more updates

--- phpinfo.php
+++ phpinfo.php
@@ -1,1 +1,9 @@
-<? phpinfo() ?>
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+?>
